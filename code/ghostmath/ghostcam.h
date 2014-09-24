24efe7511dea5121cbd04b75210dcf7cb9b8ba00
Enzo Dayrit <enzodayrit11@gmail.com>
2014-09-24 18:28:04 +0900

    re-adds Kinect parameter loading

--- code/ghostmath/ghostcam.h
+++ code/ghostmath/ghostcam.h
@@ -5,7 +5,7 @@ void setCameraMatrix(float param0, float param1, float param2, float param3, flo
 void setCameraMatrix(cv::Mat);
 //void calculateCameraMatrix();
 
-cv::Mat getCameraMatrix();
-cv::Mat getInvCameraMatrix();
+cv::Mat& getCameraMatrix();
+cv::Mat& getInvCameraMatrix();
 
 cv::Vec2f toScreen(cv::Vec3f v);
